af0586df216eca18e5af3e166acdc1d2475918e9
Abhinav5868 <abhinav2003.d@gmail.com>
2024-11-06 14:42:21 +0530

    Update Application.java

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Application {
     public String getMessage() {
-        return "Hello, CI/CD Pipeline!";
+        return "Hello :), CI/CD Pipeline!";
     }
 
     @GetMapping("/")
